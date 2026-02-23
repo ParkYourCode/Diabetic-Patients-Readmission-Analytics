b2b05502a12d24fb0ce90aaf5a31962e845da8b0
ParkYourCode <andrewcoderer@gmail.com>
2026-02-22 16:55:33 -0800

    Add categorical encoding for id fields

--- diabetes_dbt/models/01_silver/diabetes_cleaned.sql
+++ diabetes_dbt/models/01_silver/diabetes_cleaned.sql
@@ -10,13 +10,32 @@ WITH source AS (
 cleaned AS (
     SELECT
         encounter_id,
-        patient_nbr,
+        patient_nbr AS patient_id,
         race,
         gender,
         age,
-        admission_type_id,
-        discharge_disposition_id,
-        admission_source_id,
+        CASE 
+            WHEN admission_type_id = 1 THEN 'Emergency'
+            WHEN admission_type_id = 2 THEN 'Urgent'
+            WHEN admission_type_id = 3 THEN 'Elective'
+            WHEN admission_type_id = 4 THEN 'Newborn'
+            WHEN admission_type_id = 7 THEN 'Trauma Center'
+            WHEN (admission_type_id = 5 
+                OR admission_type_id = 6
+                OR admission_type_id = 8) 
+            THEN 'Not Available'
+            ELSE 'Other'
+        END AS admission_type,
+        CASE
+            WHEN discharge_disposition_id IN (1, 6, 8) THEN 'Home'
+            WHEN discharge_disposition_id IN (2, 3, 4, 5, 22, 23, 24, 27, 28, 29, 30) THEN 'Transfer'
+            ELSE 'Other'
+        END AS discharge_disposition,
+        CASE
+            WHEN admission_source_id IN (7) THEN 'Emergency'
+            WHEN admission_source_id IN (1, 2, 3) THEN 'Referral'
+            WHEN admission_source_id IN (4, 5, 6, 10, 22, 25) THEN 'Transfer'
+        END AS admission_source,
         time_in_hospital,
         num_lab_procedures,
         num_procedures,
@@ -51,7 +70,10 @@ cleaned AS (
         glimepiride_pioglitazone,
         metformin_rosiglitazone,
         metformin_pioglitazone,
-        change,
+        CASE
+            WHEN change = 'Yes' THEN 1
+            ELSE 0
+        END AS medication_changed,
         diabetesMed,
         readmitted,
         CASE
@@ -59,10 +81,11 @@ cleaned AS (
             ELSE 0
         END AS readmitted_30_days
     FROM source
-    WHERE discharge_disposition_id NOT IN (11, 13, 14, 18, 19, 20, 21)
-    AND (diag_1 != '?' OR diag_2 != '?' OR diag_3 != '?')
-    AND gender != 'Unknown/Invalid'
-    AND race != '?'
+    WHERE discharge_disposition_id NOT IN (11, 13, 14, 18, 19, 20, 21, 25, 26)
+        AND admission_source_id NOT IN (8, 9, 11, 12, 13, 14, 15, 17, 20, 21, 23, 24, 26)
+        AND (diag_1 != '?' OR diag_2 != '?' OR diag_3 != '?')
+        AND gender != 'Unknown/Invalid'
+        AND race != '?'
 )
 SELECT *
 FROM cleaned
